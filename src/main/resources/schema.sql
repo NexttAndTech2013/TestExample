0d92f18be450def2a5d593229d76b557509503f5
quinteromi <quinteromi@mediamarkt.es>
2019-03-28 17:40:51 +0100

    schema pokemon

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,19 @@
+DROP SEQUENCE IF exists SQ_POKEMON;
+
+DROP TABLE IF EXISTS pokemon CASCADE;
+
+CREATE SEQUENCE SQ_POKEMON;
+
+CREATE TABLE pokemon (
+  id bigint not null UNIQUE,
+  nombre varchar not null,
+  sexo varchar not null,
+  ataque int not null,
+  defensa int not null,
+  velocidad int not null,
+  vida int not null,
+  fecha_captura date not null,
+  legendario boolean false not null,
+  pc int not null,
+  tipo varchar not null
+);
