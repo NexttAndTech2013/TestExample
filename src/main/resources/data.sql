204cec3acc74468703719822387069a6e45fa8fd
gonzalezait <gonzalezait@mediamarkt.es>
2019-03-29 12:42:27 +0100

    Algunos datos de ejemplo

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.pokemon(
+  id, nombre, sexo, ataque, defensa, velocidad, vida, fecha_captura, legendario, pc, tipo)
+VALUES (1,'Pikachu','M', 100, 100, 100, 100, '29-03-2019', true, 100, 'Eléctrico');
+INSERT INTO public.pokemon(
+  id, nombre, sexo, ataque, defensa, velocidad, vida, fecha_captura, legendario, pc, tipo)
+VALUES (2,'Bulbasaur','H', 150, 200, 50, 100, '27-03-2019', false, 89, 'Planta');
+INSERT INTO public.pokemon(
+  id, nombre, sexo, ataque, defensa, velocidad, vida, fecha_captura, legendario, pc, tipo)
+VALUES (3,'Charmander','H', 200, 300, 150, 200, '16-03-2019', true, 147, 'Fuego');
+INSERT INTO public.pokemon(
+  id, nombre, sexo, ataque, defensa, velocidad, vida, fecha_captura, legendario, pc, tipo)
+VALUES (4,'Hitmonlee','M', 300, 120, 270, 250, '28-02-2019', false, 100, 'Lucha');
+INSERT INTO public.pokemon(
+  id, nombre, sexo, ataque, defensa, velocidad, vida, fecha_captura, legendario, pc, tipo)
+VALUES (5,'Blastoise','H', 300, 120, 270, 250, '02-03-2019', false, 347, 'Agua');
